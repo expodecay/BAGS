c71b5266972ee3092ad9a968844acb2fb5c4e2cf
Rick Ramirez <Rick.ramirez6174@gmail.com>
2020-09-16 00:04:18 -0700

    created package

--- src/DeleteMe/OnlineShopper.java
+++ src/DeleteMe/OnlineShopper.java
@@ -10,8 +10,8 @@ public class OnlineShopper {
                         new Item("Bird bath", 4499),
                         new Item("Sunflower seeds", 1295)
                 };
-        //DeleteMe.BagInterface<DeleteMe.Item> shoppingCart = new FixedSizeArrayBag<>();
-        //DeleteMe.BagInterface<DeleteMe.Item> shoppingCart = new ResizableArrayBag<>();
+        //DeleteMe.CS_2400_Data_Structures_and_Advanced_Programming.BagInterface<DeleteMe.CS_2400_Data_Structures_and_Advanced_Programming.Item> shoppingCart = new FixedSizeArrayBag<>();
+        //DeleteMe.CS_2400_Data_Structures_and_Advanced_Programming.BagInterface<DeleteMe.CS_2400_Data_Structures_and_Advanced_Programming.Item> shoppingCart = new CS_2400_Data_Structures_and_Advanced_Programming.ResizableArrayBag<>();
         BagInterface<Item> shoppingCart = new LinkedBag<>();
         int totalCost = 0;
         //Statements that add selected items to the shopping cart:
